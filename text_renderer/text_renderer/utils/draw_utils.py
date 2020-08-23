be0f5c5b73d737696eef69e463de834adb991488
java-abhinav07 <java.abhinav99@gmail.com>
2020-08-23 20:00:03 +0530

    testing code

--- text_renderer/text_renderer/utils/draw_utils.py
+++ text_renderer/text_renderer/utils/draw_utils.py
@@ -76,8 +76,7 @@ def draw_text_on_bg(
         top_left = (c_x, c_y - y_offset)
         bottom_right = (c_x + chars_size[i][0] + char_spacings[i], c_y)
         draw.text(top_left, c, fill=text_color, font=font_text.font)
-        draw.rectangle((top_left, bottom_right), outline=(0,0,0), width=3)
-        print("drawing..")
+        draw.rectangle((top_left, bottom_right))
         c_x += chars_size[i][0] + char_spacings[i]
 
     return text_mask
