8bdbf11843ab25a18685468d6e804b518219c60c
java-abhinav07 <java.abhinav99@gmail.com>
2020-08-27 03:43:00 +0530

    tuning params

--- aocr/defaults.py
+++ aocr/defaults.py
@@ -31,24 +31,24 @@ class Config(object):
     FULL_ASCII = False
 
     # Optimization
-    NUM_EPOCH = 32
+    NUM_EPOCH = 20
     BATCH_SIZE = 64
     INITIAL_LEARNING_RATE = 1.0
 
     # Network parameters
     CLIP_GRADIENTS = True  # whether to perform gradient clipping
     MAX_GRADIENT_NORM = 5.0  # Clip gradients to this norm
-    TARGET_EMBEDDING_SIZE = 16  # embedding dimension for each target
-    ATTN_NUM_HIDDEN = 96  # number of hidden units in attention decoder cell
-    ATTN_NUM_LAYERS = 2  # number of layers in attention decoder cell
+    TARGET_EMBEDDING_SIZE = 64  # embedding dimension for each target
+    ATTN_NUM_HIDDEN = 256  # number of hidden units in attention decoder cell
+    ATTN_NUM_LAYERS = 4  # number of layers in attention decoder cell
     # (Encoder number of hidden units will be ATTN_NUM_HIDDEN*ATTN_NUM_LAYERS)
     LOAD_MODEL = True
     OLD_MODEL_VERSION = False
     TARGET_VOCAB_SIZE = 37  # 0: PADDING, 1: GO, 2: EOS, >2: 0-9, a-z
     CHANNELS = 1  # number of color channels from source image (1 = grayscale, 3 = rgb)
 
-    MAX_WIDTH = 1000 # images with higher width will be discarded!!! --> fix now
-    MAX_HEIGHT = 200
+    MAX_WIDTH = 2000 # images with higher width will be discarded!!! --> fix now
+    MAX_HEIGHT = 1000
     MAX_PREDICTION = 50
 
     USE_DISTANCE = True
