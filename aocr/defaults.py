d0966968698382fef7d31531170a00af64179d9a
java-abhinav07 <java.abhinav99@gmail.com>
2020-08-27 13:43:49 +0530

    modified params

--- aocr/defaults.py
+++ aocr/defaults.py
@@ -16,7 +16,7 @@ class Config(object):
 
     # I/O
     NEW_DATASET_PATH = "./dataset.tfrecords"
-    DATA_PATH = "/content/drive/My\ Drive/IITB_Assignment/checkpoints_2020-08-26 23:29:15.806049"
+    DATA_PATH = "/content/drive/My\ Drive/IITB_Assignment/datasets/ds0/training.tfrecords"
     # MODEL_DIR = f"./checkpoints_{time}"
     MODEL_DIR = (
         "/content/drive/My Drive/IITB_Assignment/checkpoints_2020-08-27 06:25:03.598532"
