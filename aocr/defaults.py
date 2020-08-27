13256ec23a367dabf2867fda17de71a5c426c2b3
java-abhinav07 <java.abhinav99@gmail.com>
2020-08-27 19:12:42 +0530

    adding images

--- aocr/defaults.py
+++ aocr/defaults.py
@@ -17,10 +17,10 @@ class Config(object):
     # I/O
     NEW_DATASET_PATH = "./dataset.tfrecords"
     DATA_PATH = "/content/drive/My\ Drive/IITB_Assignment/datasets/ds0/training.tfrecords"
-    # MODEL_DIR = f"./checkpoints_{time}"
-    MODEL_DIR = (
-        "/content/drive/My Drive/IITB_Assignment/checkpoints_2020-08-27 06:25:03.598532"
-    )
+    MODEL_DIR = f"./checkpoints_{time}"
+    # MODEL_DIR = (
+    #     "/content/drive/My Drive/IITB_Assignment/checkpoints_2020-08-27 06:25:03.598532"
+    # )
     LOG_PATH = f"aocr_{time}.log"
     OUTPUT_DIR = "/content/drive/My\ Drive/IITB_Assignment/results"
     STEPS_PER_CHECKPOINT = 500
@@ -39,16 +39,16 @@ class Config(object):
     CLIP_GRADIENTS = True  # whether to perform gradient clipping
     MAX_GRADIENT_NORM = 5.0  # Clip gradients to this norm
     TARGET_EMBEDDING_SIZE = 16  # embedding dimension for each target
-    ATTN_NUM_HIDDEN = 256  # number of hidden units in attention decoder cell
+    ATTN_NUM_HIDDEN = 160  # number of hidden units in attention decoder cell
     ATTN_NUM_LAYERS = 2  # number of layers in attention decoder cell
     # (Encoder number of hidden units will be ATTN_NUM_HIDDEN*ATTN_NUM_LAYERS)
     LOAD_MODEL = True
     OLD_MODEL_VERSION = False
-    TARGET_VOCAB_SIZE = 30  # 0: PADDING, 1: GO, 2: EOS, >2: 0-9, a-z
+    TARGET_VOCAB_SIZE = 40  # 0: PADDING, 1: GO, 2: EOS, >2: 0-9, a-z
     CHANNELS = 1  # number of color channels from source image (1 = grayscale, 3 = rgb)
 
-    MAX_WIDTH = 2000 # images with higher width will be discarded!!! --> fix now
-    MAX_HEIGHT = 100
+    MAX_WIDTH = 330 # images with higher width will be discarded!!! --> fix now
+    MAX_HEIGHT = 40
     MAX_PREDICTION = 50
 
     USE_DISTANCE = True
