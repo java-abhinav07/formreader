2de8511cafa8bc73d0fd76195fc4d32703d9f96b
java-abhinav07 <java.abhinav99@gmail.com>
2020-08-31 21:52:23 +0530

    utils

--- app/app.py
+++ app/app.py
@@ -6,7 +6,7 @@ import requests
 from flask import jsonify
 from flask import Flask, request, jsonify
 
-from app.utils import *
+from utils import *
 from aocr.__main__ import main_app
 from aocr.defaults import Config
 
